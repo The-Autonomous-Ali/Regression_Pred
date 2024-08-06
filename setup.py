6f14fc5c584e523ea4f20951fb3868b1226ddd1c
The-Autonomous-Ali <sameerup1996@gmail.com>
2024-08-06 23:24:04 +0530

    Folder strucuture updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Insurance-predictior",
+    version="0.1.1",
+    author="Sameer Ali",
+    author_email="Sameerup1996@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
